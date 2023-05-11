74fe5c0500bda4c6d4349d6e1b2670c9f7fefa61
hassanbahaa <hassanbahaa45@gmail.com>
2023-05-11 06:54:06 +0200

    test api with the app

--- lib/core/class/crud.dart
+++ lib/core/class/crud.dart
@@ -7,27 +7,29 @@ import 'package:http/http.dart' as http;
 class Crud {
   Future<Either<StatusRequest,Map>> postData(String linkUrl,Map data) async{
     // check if there is internet connection
-try{
-  if(await checkInternet()){
+    try{
+      if( await checkInternet() ){
+        var response = await http.post(Uri.parse(linkUrl),body: data);
+        // if there is an internet connection
+        if( response.statusCode == 200 ){
+          Map responseBody = jsonDecode(response.body);
+          // return result body
+          return Right(responseBody);
+        }else{
+          // if there is a problem in connection check so return server fail
+          return const Left(StatusRequest.serverFailure);
+        }
 
-    var response = await http.post(Uri.parse(linkUrl),body: data);
 
-    // if there is an internet connection
-    if(response.statusCode==200 ||response.statusCode==201 ){
-      Map responseBody = jsonDecode(response.body);
-      // return result body
-      return Right(responseBody);
-    }else{
-      // if there is a problem in connection check so return server fail
+      }else{
+        return const Left(StatusRequest.offlineFailure);
+      }
+    }catch(r){
+      print("catch error  $r");
       return const Left(StatusRequest.serverFailure);
     }
-
-
-  }else{
-    return const Left(StatusRequest.offlineFailure);
-  }
-}catch(r){
-  return const Left(StatusRequest.serverFailure);
-}
   }
 }
+
+
+
--- lib/linkapi.dart
+++ lib/linkapi.dart
@@ -1,6 +1,7 @@
 class AppLink{
 
   static const String server      = "https://hamzawyappstore.000webhostapp.com";
+  // static const String server      = "https://google.com";
   static const String test        = "$server/test.php";
 
 
